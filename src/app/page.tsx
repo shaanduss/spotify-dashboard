e3d44220841dafb2395c662d82985b0d19048ddf
Shaan <shaanduss@gmail.com>
2025-11-07 16:22:42 +0800

    Theming Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="min-h-screen flex-col">
-      <Button variant="outline">Show Toast</Button>
+      <></>
     </div>
   );
 }
