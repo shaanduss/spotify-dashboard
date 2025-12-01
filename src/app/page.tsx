4f20813f14da0dd6f9c87507cddbab0066216772
Shaan <shaanduss@gmail.com>
2025-12-01 22:03:21 +0800

    Create Loading Animation when waiting for user data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@
 import { ListeningProfile } from "@/components/home/listeningProfile";
 import { MoodPieChart } from "@/components/home/moodPieChart";
 import { Top10Tracks } from "@/components/home/top10Tracks";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import { userType } from "@/schemas/userSchema";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -29,7 +31,17 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center min-h-screen w-full rounded-xl shadow-lg p-5 gap-y-10">
-      {loading || user?.spotifyLink == "" ? (
+      {loading && (
+        <div className="w-full flex justify-center">
+          <div>
+            <Badge className="text-md gap-x-3">
+              <Spinner className="!size-4" />
+              Fetching...
+            </Badge>
+          </div>
+        </div>
+      )}
+      {!loading && user?.spotifyLink == "" && (
         <div className="w-full h-full flex items-center justify-center">
           <Button
             variant="outline"
@@ -39,7 +51,9 @@ export default function Home() {
             Connect Spotify Account
           </Button>
         </div>
-      ) : (
+      )}
+
+      {!loading && user?.spotifyLink != "" && (
         <>
           <div className="flex gap-x-8 ">
             <ListeningProfile />
